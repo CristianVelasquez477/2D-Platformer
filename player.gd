947d54a92cd7e9ccbbcce36d76da5dcf86a2f96d
CristianVelasquez477 <crvelazquez@s.sfusd.edu>
2026-03-23 13:21:02 -0700

    Videos_1-9_complete

---
+++ player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+@export var accelaration : float = 50
+@export var braking : float = 20
+@export var gravity : float = 500
+@export var jump_force : float = 200
+
+var move_input : float
+
+func _physics_process(delta):
+	# gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	# get the move input
+	move_input = Input.get_axis("move_left","move_right")
+	
+	# movement
+	if move_input != 0:
+		velocity.x = lerp(velocity.x, move_input * move_speed, accelaration * delta) 
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+	else:
+		velocity.x = lerp(velocity.x, 0.0, braking * delta)
+
+	
+	move_and_slide()
+
+
+func _proccess
