a653722786708cbc9f3a51e75d760c98d945f059
CristianVelasquez477 <crvelazquez@s.sfusd.edu>
2026-04-10 13:29:28 -0700

    progress save video 20

--- Scripts/player_ui.gd
+++ Scripts/player_ui.gd
@@ -1,1 +1,18 @@
 extends CanvasLayer
+
+@onready var health_container = $HealthContainer
+var hearts : Array = []
+
+@onready var score_text : Label = $ScoreText
+
+@onready var  player = get_parent()
+
+
+func _ready ():
+	 hearts  = health_container.get_children()
+	
+	player.OnUpdateHealth.connect(_update_hearts)
+	player.OnUpdateScore.connect(_update_score)
+	
+	
+	
