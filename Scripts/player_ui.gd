4a27895dde9d9ab7f7c4dc5168801028fd36337d
CristianVelasquez477 <crvelazquez@s.sfusd.edu>
2026-04-14 12:17:34 -0700

    Update player_ui.gd
    HELP!!!!!!

--- Scripts/player_ui.gd
+++ Scripts/player_ui.gd
@@ -5,14 +5,29 @@ var hearts : Array = []
 
 @onready var score_text : Label = $ScoreText
 
-@onready var  player = get_parent()
+@onready var player = get_parent()
 
 
 func _ready ():
-	 hearts  = health_container.get_children()
+	hearts = health_container.get_children()
 	
 	player.OnUpdateHealth.connect(_update_hearts)
 	player.OnUpdateScore.connect(_update_score)
+	
+	_update_hearts(player.health)
+	_update_score(playerstats.score)
+	
+func _update_hearts (health : int):
+	for i in len(hearts):
+		hearts[i].visible = i < health
+	
+	
+	
+func  _update_score (score : int):
+	score_text.text = "Score: " + str(score)
+	
+	
+	
 	
 	
 	
